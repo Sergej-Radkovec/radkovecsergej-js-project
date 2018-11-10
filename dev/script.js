c482dc0299b1a333c69b4feaa69c163430c28790
Sergej Radkovec <radkovecsergej@gmail.com>
2018-11-10 22:31:03 +0300

    add new way unit

--- dev/script.js
+++ dev/script.js
@@ -10,7 +10,7 @@
 
   let playing = false;
   const frequency = 15;
-  const generateSpeed = 4500;
+  const generateSpeed = 4500000;
   let timeGame = 0;
 
   canvas.width = window.innerWidth;
@@ -30,9 +30,9 @@
   function game() {
     if (playing) {
       timeGame += frequency;
-      if (timeGame % generateSpeed === 0) {
+      if (timeGame % generateSpeed === 0 || timeGame === frequency) {
         generateUnit();
-        console.log(units);
+        generateUnit();
       }
       units.forEach(unit => posBall(unit));
     }
@@ -41,7 +41,7 @@
   function Unit() {
     const self = this;
     const unitSize = 50;
-    const speed = 1;
+    self.speed = 0.5;
     self.way = [];
 
     function setRandomPos() {
@@ -63,8 +63,8 @@
     }
 
     function setRandomDirection() {
-      self.speedY = Math.random() * (speed + speed / 3) - speed / 3;
-      self.speedX = Math.sqrt(speed * speed - self.speedY * self.speedY);
+      self.speedY = Math.random() * (self.speed + self.speed / 3) - self.speed / 3;
+      self.speedX = Math.sqrt(self.speed * self.speed - self.speedY * self.speedY);
       if (Math.random() > 0.5) {
         self.speedX *= -1;
       }
@@ -104,16 +104,47 @@
     units.forEach(value => value.update());
   }
 
-  // Изменяем положение обьектов без задания траектории
+  // Изменяем положение обьектов
+
   function posBall(elem) {
+    let cutLength;
+    let sin;
+    let cos;
+    let posEnd;
+
+
+    if (elem.way.length) {
+      posEnd = elem.way[0];
+      cutLength = Math.sqrt((posEnd[0] - elem.posX) * (posEnd[0] - elem.posX)
+        + (posEnd[1] - elem.posY) * (posEnd[1] - elem.posY));
+      sin = (posEnd[0] - elem.posX) / cutLength;
+      cos = (posEnd[1] - elem.posY) / cutLength;
+      if (elem.speed < cutLength) {
+        elem.speedX = sin * elem.speed;
+        elem.speedY = cos * elem.speed;
+      } else {
+        let posStart = elem.way.shift();
+        if (elem.way.length) {
+          posEnd = elem.way[0];
+          cutLength = Math.sqrt((posEnd[0] - elem.posX) * (posEnd[0] - elem.posX)
+            + (posEnd[1] - elem.posY) * (posEnd[1] - elem.posY));
+          sin = (posEnd[0] - posStart[0]) / cutLength;
+          cos = (posEnd[1] - posStart[1]) / cutLength;
+          elem.speedX = sin * (cutLength - elem.speed);
+          elem.speedX = cos * (cutLength - elem.speed);
+        }
+      }
+    }
+
     elem.posX += elem.speedX;
+    elem.posY += elem.speedY;
+
     if (elem.posX < 0) {
       elem.speedX *= -1;
     }
     if (elem.posX > window.innerWidth) {
       elem.speedX *= -1;
     }
-    elem.posY += elem.speedY;
     if (elem.posY < 0) {
       elem.speedY *= -1;
     }
@@ -124,16 +155,25 @@
   }
   // добавление траектории
   document.addEventListener('mousedown', (event) => {
-    document.addEventListener('mousemove', setWay);
-    function setWay(e) {
-      units.forEach((unit) => {
-        if (event.target === unit.obj) {
-          let x = e.pageX;
-          let y = e.pageY;
-          unit.way.push([x, y]);
-        }
-      });
-    }
-    document.addEventListener('mouseup', () => document.removeEventListener('mousemove', setWay));
+    event = event || window.event;
+    event.preventDefault();
+
+    units.forEach(unit => {
+      if (event.target === unit.obj) {
+        document.addEventListener('mousemove', setWay);
+      }
+    });
+    document.addEventListener('mouseup', () => {document.removeEventListener('mousemove', setWay)});
   });
+
+  function setWay(e) {
+    e = e || window.event;
+    e.preventDefault();
+    let x = e.pageX;
+    let y = e.pageY;
+    unit.way.push([x, y]);
+  }
 })();
+
+
+
